b06ee294139826168f7755eaf5c25835a2bf210e
Tamiris Oliveira <116034830+Tamirisoliveira12@users.noreply.github.com>
2023-01-04 18:02:17 -0300

    atualização

---
+++ Js2/aula03.js
@@ -0,0 +1,19 @@
+"use strict"
+
+function teste(){
+    let nome="Bruno"
+    if(true){
+        console.log("dentro de if teste: " + nome)
+    }
+    console.log("dentro de teste: " + nome)
+
+}
+
+let nome2 ="paula"
+nome2=10
+console.log(nome2)
+
+const nome3= "tami"
+nome3=10 //const n pode ser alterada
+console.log(nome3)
+
---
+++ Js2/aula04.js
@@ -0,0 +1,6 @@
+let num1=0, num2=0, res=0
+
+num1=2
+num1++
+
+console.log(num1)
