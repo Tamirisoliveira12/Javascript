65ae824b3724bd735629767b5667f024ede06a81
Tamiris Oliveira <116034830+Tamirisoliveira12@users.noreply.github.com>
2023-01-18 17:20:03 -0300

    atualização das aulas js

---
+++ Js2/aula22.js
@@ -0,0 +1,29 @@
+const VALOR_PADRAO=0
+
+
+function add(v){
+    return valor+v
+}
+
+let valor=0
+console.log(valor)
+
+valor=add(10)
+console.log(valor)
+
+valor=add(50)
+console.log(valor)
+
+
+
+
+/*function soma(n1=VALOR_PADRAO,n2=VALOR_PADRAO){
+    let res
+    res=n1+n2
+    return res
+}
+
+let resultado_soma=soma(5,5)
+console.log(resultado_soma)*/
+
+
