adb1e81d000724c847a709ec9342fdb3fca96466
Tamiris Oliveira <116034830+Tamirisoliveira12@users.noreply.github.com>
2023-02-13 12:03:24 -0300

    atualização das aulas

---
+++ Js2/aula24.js
@@ -0,0 +1,12 @@
+const f=new Function("v1","v2","v3","return v1+v2+v3")  //função construtor anonima
+
+console.log(f(10,5,15))
+
+
+
+
+/**const f=function(v1,v2){
+   return v1+v2
+}
+
+console.log(f(10,5))**/
---
+++ Js2/aula25.js
@@ -0,0 +1,8 @@
+//const soma=function(v1,v2)(return v1+v2)
+
+const soma=(v1,v2)=>{return v1+v2}
+
+const name=n=>{return n}
+
+console.log(soma(10,5))
+console.log(name("Bruno"))
